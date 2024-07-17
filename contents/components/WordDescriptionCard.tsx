604948c426fc8800e5fedac044ca501bb0008d97
Yuta-31 <pineapple.gt@gmail.com>
2024-07-17 23:02:54 +0900

    fix: word card design

--- contents/components/WordDescriptionCard.tsx
+++ contents/components/WordDescriptionCard.tsx
@@ -7,16 +7,7 @@ export const WordDescriptionCard = () => {
   const word = useSelector((state: WordState) => state.word);
   const rect = useSelector((state: WordState) => state.rect);
 
-  const calcLeft = () => {
-    const windowWidth = window.innerWidth;
-    console.log(windowWidth);
-    if (rect.cursorX + 400 > windowWidth) {
-      return rect.cursorX + rect.scrollX - 400;
-    }
-    return rect.cursorX + rect.scrollX - 20;
-  };
-
-  const calcTop = () => {
+  const calcCardTop = () => {
     console.log(rect);
     if (rect.cursorY < 200) {
       // if the word is at the top of the page
@@ -37,13 +28,40 @@ export const WordDescriptionCard = () => {
         // if the word is wrapped
         if (rect.cursorX > rect.contentLeft + rect.contentWidth / 2) {
           // if the word is at the right side of the page
-          return rect.top + rect.scrollY - rect.height - 90;
+          return rect.top + rect.scrollY - rect.height - 130;
         } else {
           // if the word is at the left side of the page
-          return rect.top + rect.scrollY - rect.height - 65;
+          return rect.top + rect.scrollY - rect.height - 105;
         }
       }
-      return rect.top + rect.scrollY - 135;
+      return rect.top + rect.scrollY - 175;
+    }
+  };
+
+  const calcCardLeft = () => {
+    const windowWidth = window.innerWidth;
+    console.log(windowWidth);
+    if (rect.cursorX + 400 > windowWidth) {
+      return rect.cursorX + rect.scrollX - 400;
+    }
+    return rect.cursorX + rect.scrollX - 20;
+  };
+
+  const calcTipTop = () => {
+    if (rect.cursorY < 200) {
+      // if the word is at the top of the page
+      return -9;
+    } else {
+      return 150 + 18;
+    }
+  };
+
+  const calcTipLeft = () => {
+    const windowWidth = window.innerWidth;
+    if (rect.cursorX + 400 > windowWidth) {
+      return 400 * 0.98;
+    } else {
+      return 400 * 0.02;
     }
   };
 
@@ -54,15 +72,26 @@ export const WordDescriptionCard = () => {
     <div
       className="mldi_word_card"
       style={{
-        position: 'absolute',
-        top: calcTop(),
-        left: calcLeft()
+        position: 'relative',
+        top: calcCardTop(),
+        left: calcCardLeft()
       }}
     >
-      <h1>
-        {word} {rect.height}
-      </h1>
-      <p>This is a word description card</p>
+      <div className="word">{word}</div>
+      <div className="description">This is a word description card</div>
+      <div className="tags">#Microsoft365 #Skype</div>
+      <div
+        className="bubble_tip"
+        style={{
+          position: 'absolute',
+          top: calcTipTop(),
+          left: calcTipLeft(),
+          clipPath:
+            rect.cursorY < 200
+              ? 'polygon(0 100%, 50% 0, 100% 100%)'
+              : 'polygon(0 0, 50% 100%, 100% 0)'
+        }}
+      />
     </div>
   );
 };
