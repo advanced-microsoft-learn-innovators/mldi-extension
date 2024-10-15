e8c46c09ae596dba13498269ed31d4a35b12d20e
Yuta-31 <pineapple.gt@gmail.com>
2024-10-15 23:19:27 +0900

    feat: create new api to get content

--- background/messages/api.ts
+++ background/messages/api.ts
@@ -10,61 +10,86 @@ const handleApi = (
   sendResponse: (response?: any) => void
 ) => {
   switch (message.command) {
-    case 'fetchSummary':
-      // fetch summary
+    case 'fetchDocument':
+      // fetch document
       (async () => {
-        // get document_id and uuid
         const [tab] = await chrome.tabs.query({
           active: true,
           lastFocusedWindow: true
         });
+
+        // load 'isSummaryHeadeingLevels' setting from storage
+        const isSummaryHeadeingLevels = (await storage.get(
+          'isSummaryHeadeingLevels'
+        )) || { h2: false, h3: false, h4: false };
+
+        // get document_id and uuid
         const res = await chrome.tabs.sendMessage(tab.id, {
           type: 'getContentData',
-          command: 'getDucumentIds',
+          command: 'getDocumentIds',
           data: {}
         });
         const documentId = res.documentId;
         const uuid = res.uuid;
+        const url = res.url;
 
-        // get url
-        const url = message.data.url;
-
-        // get heading level setting
-        const isSummaryHeadeingLevels = (await storage.get(
-          'isSummaryHeadeingLevels'
-        )) || { h2: false, h3: false, h4: false };
-        console.log(isSummaryHeadeingLevels);
-
-        // cannot set summarySectionLevels parameter in "params", beacuse cannot set same key multiple times
+        // fetch contents
         const response: AxiosResponse = await axios.get(
-          `${process.env.PLASMO_PUBLIC_BACKEND_DOMAIN}/documents/${documentId}/summary-contents/${uuid}?url=https://learn.microsoft.com/ja-jp/deployedge/microsoft-edge-channels&summarySectionLevels=${isSummaryHeadeingLevels['h2']}&summarySectionLevels=${isSummaryHeadeingLevels['h3']}&summarySectionLevels=${isSummaryHeadeingLevels['h4']}`
+          `${process.env.PLASMO_PUBLIC_BACKEND_DOMAIN}/documents/${documentId}/document-contents/${uuid}?url=${url}&summarySectionLevels=${isSummaryHeadeingLevels['h2']},${isSummaryHeadeingLevels['h3']},${isSummaryHeadeingLevels['h4']}`
         );
-        const { data, status } = response;
 
-        let headingSummaries = {};
-        if (data?.headingSection.length > 0) {
-          console.log(data);
-          // ensure data might be undefined
-          data.headingSection.forEach((section) => {
-            headingSummaries[section.id] = section.sectionSummary;
-          });
+        switch (response.status) {
+          case 200:
+            // if the content exists and is latest.
+            // send summary to content script
+            let headingSummaries = {};
+            if (response.data?.headingSection.length > 0) {
+              console.log(response.data);
+              // ensure data might be undefined
+              response.data.headingSection.forEach((section) => {
+                headingSummaries[section.id] = section.sectionSummary;
+              });
+            }
+
+            await chrome.tabs.sendMessage(tab.id, {
+              type: 'response',
+              command: 'fetchSectionSummary',
+              data: {
+                sectionSummaries: headingSummaries
+              }
+            });
+            await chrome.tabs.sendMessage(tab.id, {
+              type: 'response',
+              command: 'fetchSummary',
+              data: {
+                summary: response.data.mainSummary
+              }
+            });
+
+            // TODO: send word description to content script
+
+            break;
+          case 404:
+            // if the content does not exist or is not latest.
+            const postResponse: AxiosResponse = await axios.post(
+              `${process.env.PLASMO_PUBLIC_BACKEND_DOMAIN}/documents/${documentId}/document-contents`,
+              {
+                url: url,
+                summarySectionLevels: isSummaryHeadeingLevels
+              }
+            );
+            const { data, status } = postResponse;
+            if (status === 201) {
+              break;
+            } else {
+              console.error('Failed to create document content', status);
+              break;
+            }
+          default:
+            break;
         }
-
-        await chrome.tabs.sendMessage(tab.id, {
-          type: 'response',
-          command: 'fetchSectionSummary',
-          data: {
-            sectionSummaries: headingSummaries
-          }
-        });
-        await chrome.tabs.sendMessage(tab.id, {
-          type: 'response',
-          command: 'fetchSummary',
-          data: {
-            summary: data.mainSummary
-          }
-        });
       })();
+      return;
     case 'fetchWordList':
       // fetch word list
       (async () => {
--- contents/content.ts
+++ contents/content.ts
@@ -4,6 +4,7 @@ const getDocumentIds = () => {
   const headChildren = document.head.children;
   let documentId = '';
   let uuid = '';
+  let url = '';
 
   for (var i = 0; i < headChildren.length; i++) {
     var tmp = headChildren[i].getAttribute('name');
@@ -13,17 +14,20 @@ const getDocumentIds = () => {
       uuid = headChildren[i].getAttribute('content');
     }
   }
-  return [documentId, uuid];
+
+  url = window.location.href;
+  return [documentId, uuid, url];
 };
 
 window.addEventListener('load', async () => {
   console.log('content.ts loaded');
   chrome.runtime.onMessage.addListener(
     (message: Message, sender, sendResponse) => {
-      const [documentId, uuid] = getDocumentIds();
+      const [documentId, uuid, url] = getDocumentIds();
       sendResponse({
         documentId,
-        uuid
+        uuid,
+        url
       });
     }
   );
