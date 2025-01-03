dc81e9fb5a5d73dfb0b87d30598709712761a206
Yuta-31 <pineapple.gt@gmail.com>
2025-01-03 18:36:39 +0900

    chore: separate apis

---
+++ background/messages/apis/fetchSummary.ts
@@ -0,0 +1,80 @@
+import { Logger } from '~utils';
+import { storage } from 'background';
+import type { AxiosResponse } from 'axios';
+import axios from 'axios';
+import type { Message } from '~types';
+
+export const fetchSummary = async (message: Message) => {
+  // get document_id and uuid
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+  });
+  const res = await chrome.tabs.sendMessage(tab.id, {
+    type: 'getContentData',
+    command: 'getDucumentIds',
+    data: {}
+  });
+  const documentId = res.documentId;
+  const uuid = res.uuid;
+
+  // get url
+  const url = message.data.url;
+
+  // get heading level setting
+  const isSummaryHeadeingLevels = (await storage.get(
+    'isSummaryHeadeingLevels'
+  )) || { h2: false, h3: false, h4: false };
+
+  // cannot set summarySectionLevels parameter in "params", beacuse cannot set same key multiple times
+  Logger.info(`documentId: ${documentId}`);
+  Logger.info(`url: ${url}`);
+  Logger.info(`uuid: ${uuid}`);
+  Logger.info(`isSummaryHeadeingLevels.h2: ${isSummaryHeadeingLevels['h2']}`);
+  Logger.info(`isSummaryHeadeingLevels.h3: ${isSummaryHeadeingLevels['h3']}`);
+  Logger.info(`isSummaryHeadeingLevels.h4: ${isSummaryHeadeingLevels['h4']}`);
+  const restApiUrl = `${process.env.PLASMO_PUBLIC_BACKEND_DOMAIN}/documents/${documentId}/summary-contents/${uuid}?url=${url}&summarySectionLevels=${isSummaryHeadeingLevels['h2']}&summarySectionLevels=${isSummaryHeadeingLevels['h3']}&summarySectionLevels=${isSummaryHeadeingLevels['h4']}`;
+  Logger.info(`restApiUrl: ${restApiUrl}`);
+  const sendResponse = async (
+    status: number,
+    mainSummary: string,
+    sectionSummaries: any
+  ) => {
+    await chrome.tabs.sendMessage(tab.id, {
+      type: 'response',
+      command: 'fetchSectionSummary',
+      data: {
+        status: status,
+        sectionSummaries: sectionSummaries
+      }
+    });
+    await chrome.tabs.sendMessage(tab.id, {
+      type: 'response',
+      command: 'fetchSummary',
+      data: {
+        status: status,
+        summary: mainSummary
+      }
+    });
+  };
+
+  try {
+    const response: AxiosResponse = await axios.get(restApiUrl);
+    const { data, status } = response;
+    Logger.info(`status: ${status}`);
+    let headingSummaries = {};
+    if (data?.headingSection.length > 0) {
+      // ensure data might be undefined
+      data.headingSection.forEach((section) => {
+        headingSummaries[section.id] = section.sectionSummary;
+      });
+
+      await sendResponse(status, data.mainSummary, headingSummaries);
+    }
+  } catch (error) {
+    Logger.error(`fetchSummary error: ${error}`);
+    await sendResponse(500, '', '');
+  }
+
+  Logger.info('fetchSummary response');
+};
